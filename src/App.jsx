1a08c2b4dbaf43afa6951be477e8f2fd5b8b4294
DHRUV KHANDELWAL <99539203+DHRUVKHANDELWAL00@users.noreply.github.com>
2023-05-23 09:24:47 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Starter from './tutorial/06-forms/starter/02-user-challenge.jsx'
+function App() {
+  return (
+    <div className='container'>
+      <h2>Advanced React</h2>
+      <Starter />
+    </div>
+  );
+}
+
+export default App;
